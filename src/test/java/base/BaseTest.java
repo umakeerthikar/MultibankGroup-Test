c93f0476731a0ac3885219c4f6b89dd606cd3bfe
Uma Keerthika <umakeerthikar@gmail.com>
2026-03-26 00:05:04 +0530

    Updated TestData.xlsx added validSignupTest and invalidSignupTest

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -2,20 +2,26 @@ package base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.*;
-import utils.ConfigReader;
-
-import java.time.Duration;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseTest {
+
     protected WebDriver driver;
 
     @BeforeMethod
     public void setUp() {
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
 
-        driver.get(ConfigReader.get("url"));
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.get("https://trade.multibank.io/");
     }
 
     @AfterMethod
--- src/test/java/pages/SignupPage.java
+++ src/test/java/pages/SignupPage.java
@@ -1,6 +1,49 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.time.Duration;
+
 public class SignupPage {
 
+    WebDriver driver;
+    WebDriverWait wait;
 
+    public SignupPage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    // Locators
+    //*[@id="__next"]/div/div/div/div[1]/div/div/form/div[3]/span/a
+    By signUpBtn = By.xpath("//*[@id=\"__next\"]/div/div/div/div[1]/div/div/form/div[3]/span/a");
+
+    By email = By.xpath("//*[@id=\":r2:\"]");
+    By password = By.xpath("://*[@id=\":r3:\"]");
+    By certifyCheckbox = By.xpath("//*[@id=\"headlessui-switch-:r4:\"]/div/svg");
+    By nextButton = By.xpath("//*[@id=\"__next\"]/div/div/div/div[1]/div[1]/div/form/button");
+
+    // Actions
+    public void clickSignUp() {
+        wait.until(ExpectedConditions.elementToBeClickable(signUpBtn)).click();
+    }
+
+    public void enterEmail(String mail) {
+        driver.findElement(email).sendKeys(mail);
+    }
+
+    public void enterPassword(String pass) {
+        driver.findElement(password).sendKeys(pass);
+    }
+
+    public void clickcertifyCheckbox() {
+        driver.findElement(certifyCheckbox).click();
+    }
+
+    public void clickNext() {
+        driver.findElement(nextButton).click();
+    }
 }
